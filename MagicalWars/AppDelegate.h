86a90ae83ce584701421bb32773effde6061730a
Bullet <405112403@qq.com>
2013-07-21 16:22:22 +0800

    Initial Commit

---
+++ MagicalWars/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  MagicalWars
+//
+//  Created by BulletHermit on 13-7-21.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ MagicalWars/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MagicalWars
+//
+//  Created by BulletHermit on 13-7-21.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
